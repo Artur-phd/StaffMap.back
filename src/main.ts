883712d44b229c83c3158a3c3f4d912e34edd804
Artur-phd <artur.ru.2018@mail.ru>
2024-11-14 10:41:54 +0300

    feat: created swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infra/IoCC/app.module';
 import { AppFactory } from './app.factory';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = AppFactory.logger;
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule, { logger });
+  const config = new DocumentBuilder()
+    .setTitle('Notes API')
+    .setDescription('The notes API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await new AppFactory(app).useGlobalPipes().useCors().listen();
 }
 bootstrap();
